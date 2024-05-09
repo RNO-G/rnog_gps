8832b3f24ec792f296f5d171b7b538affe9fbf44
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2024-05-09 14:40:31 -0500

    baud rate fix

--- startGPSdRaw.sh
+++ startGPSdRaw.sh
@@ -20,7 +20,7 @@ then
   if [[ $rev == "F" ]] 
   then 
     echo "Changing baud rate" 
-    ubxtool -s 9600 -S 115200 -f  /dev/ttyGPS-USB
+    ubxtool -s 38400 -S 115200 -f  /dev/ttyGPS
     stty -F /dev/ttyGPS 115200
     stty -F /dev/ttyGPS-USB 115200
   fi
