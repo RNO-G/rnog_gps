b03c65a40b9d959bef45a476dcb4c4be1238fc85
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2025-08-02 00:55:42 -0500

    playing around

--- setupUblox.sh
+++ setupUblox.sh
@@ -7,18 +7,17 @@ echo "Doing ::$dev"
 ubxtool -d NMEA ::$dev #disable NMEA messages
 ubxtool -e BINARY ::$dev #enable binary messages
 
-ubxtool -e BEIDOU  ::$dev
-ubxtool -e GALILEO  ::$dev
+ubxtool -d BEIDOU  ::$dev
+ubxtool -d GALILEO  ::$dev
 ubxtool -e GLONASS ::$dev
-ubxtool -e SBAS ::$dev
+ubxtool -d SBAS ::$dev
 ubxtool -e GPS ::$dev #enable only GPS constellation
 
 #ubxtool -p CFG-GNSS #confirm enabled constellations
 ubxtool -e RAWX ::$dev #enable raw measurement messages
 ubxtool -e PPS ::$dev #enable timpulse0 [pps]
 ubxtool -e TP  ::$dev #enable tim-tp message
-ubxtool -e NAV-PVT ::$dev
-#ubxtool -p MON-SPAN  ::$dev #enable tim-tp message
+ubxtool -e NAV-SVIN  ::$dev #enable tim-tp message
 
 #ubxtool -w 4 | fgrep RAWX #confirm RAW messaging
 #ubxtool -w 4 | fgrep TP
