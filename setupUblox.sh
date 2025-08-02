8a5ed8f1890c5c57f1382a5d8b9a67f7d89ef79b
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2025-08-02 00:42:45 -0500

    better device spec

--- setupUblox.sh
+++ setupUblox.sh
@@ -3,20 +3,20 @@
 
 for dev in /dev/ttyZ9P /dev/ttyGPS ; do
 
-ubxtool -d NMEA -f $dev #disable NMEA messages
-ubxtool -e BINARY  -f $dev #enable binary messages
+ubxtool -d NMEA ::$dev #disable NMEA messages
+ubxtool -e BINARY ::$dev #enable binary messages
 
-ubxtool -e BEIDOU -f $dev
-ubxtool -e GALILEO -f $dev
-ubxtool -e GLONASS -f $dev
-ubxtool -e SBAS -f $dev
-ubxtool -e GPS -f $dev #enable only GPS constellation
+ubxtool -e BEIDOU  ::$dev
+ubxtool -e GALILEO  ::$dev
+ubxtool -e GLONASS ::$dev
+ubxtool -e SBAS ::$dev
+ubxtool -e GPS ::$dev #enable only GPS constellation
 
 #ubxtool -p CFG-GNSS #confirm enabled constellations
-ubxtool -e RAWX  -f $dev #enable raw measurement messages
-ubxtool -e PPS  -f $dev #enable timpulse0 [pps]
-ubxtool -e TP  -f $dev #enable tim-tp message
-ubxtool -e MON-SPAN  -f $dev #enable tim-tp message
+ubxtool -e RAWX ::$dev #enable raw measurement messages
+ubxtool -e PPS ::$dev #enable timpulse0 [pps]
+ubxtool -e TP  ::$dev #enable tim-tp message
+ubxtool -p MON-SPAN  ::$dev #enable tim-tp message
 
 #ubxtool -w 4 | fgrep RAWX #confirm RAW messaging
 #ubxtool -w 4 | fgrep TP
