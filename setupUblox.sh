7ac6a53442a809dbca7e898c635b9979b6692a92
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2025-08-02 00:45:28 -0500

    add printout

--- setupUblox.sh
+++ setupUblox.sh
@@ -3,6 +3,7 @@
 
 for dev in /dev/ttyZ9P /dev/ttyGPS ; do
 
+echo "Doing ::$dev"
 ubxtool -d NMEA ::$dev #disable NMEA messages
 ubxtool -e BINARY ::$dev #enable binary messages
 
@@ -16,7 +17,7 @@ ubxtool -e GPS ::$dev #enable only GPS constellation
 ubxtool -e RAWX ::$dev #enable raw measurement messages
 ubxtool -e PPS ::$dev #enable timpulse0 [pps]
 ubxtool -e TP  ::$dev #enable tim-tp message
-ubxtool -p MON-SPAN  ::$dev #enable tim-tp message
+#ubxtool -p MON-SPAN  ::$dev #enable tim-tp message
 
 #ubxtool -w 4 | fgrep RAWX #confirm RAW messaging
 #ubxtool -w 4 | fgrep TP
