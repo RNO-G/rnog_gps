4f911b53a59940f2b45c390c6102c6244d9e01cc
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2025-08-02 00:56:51 -0500

    grr

--- setupUblox.sh
+++ setupUblox.sh
@@ -17,7 +17,7 @@ ubxtool -e GPS ::$dev #enable only GPS constellation
 ubxtool -e RAWX ::$dev #enable raw measurement messages
 ubxtool -e PPS ::$dev #enable timpulse0 [pps]
 ubxtool -e TP  ::$dev #enable tim-tp message
-ubxtool -e NAV-SVIN  ::$dev #enable tim-tp message
+#ubxtool -e NAV-SVIN  ::$dev #enable tim-tp message
 
 #ubxtool -w 4 | fgrep RAWX #confirm RAW messaging
 #ubxtool -w 4 | fgrep TP
